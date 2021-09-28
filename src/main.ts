fc9bd8e8a5cfae1b6e3076cfca1ed91c18fcc31d
pjn141414 <jina071049@gmail.com>
2021-09-28 10:40:47 +0900

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('MeoGu-Bank Server API')
+    .setDescription('머구은행 서버 API')
+    .setVersion('1.0.0')
+    .addTag('')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT Token',
+        in: 'header',
+      },
+      'accesskey',
+    )
+
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(8080);
 }
 bootstrap();
