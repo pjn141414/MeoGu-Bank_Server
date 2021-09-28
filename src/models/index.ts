8474a91ba298701815a8fccbd7a9aa548505b9a2
pjn141414 <jina071049@gmail.com>
2021-09-28 23:32:11 +0900

    Fix: add model index content

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,13 @@
-export default [
+import Account from "./account";
+import EasyPassword from "./easyPassword";
+import Receive from "./receive";
+import Send from "./send";
+import User from "./User";
 
+export default [
+  User,
+  EasyPassword,
+  Account,
+  Send,
+  Receive,
 ]
