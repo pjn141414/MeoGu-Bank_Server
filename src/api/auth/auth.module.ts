4d80f549f6a99a8f5167020e7de6c18f6065972c
pjn141414 <jina071049@gmail.com>
2021-10-17 19:20:26 +0900

    Fix: userRepository file path

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import UserRepository from './repositories/user.repository';
+import UserRepository from '../user/repositories/user.repository';
 import EasyPasswordRepository from './repositories/easyPassword.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import User from 'src/models/User';
