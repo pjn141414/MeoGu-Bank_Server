d11d85bbd46d95c4d61795158692aed3c1863bac
pjn141414 <jina071049@gmail.com>
2021-10-27 20:46:01 +0900

    Add: accountNumber, name, pay

--- src/api/account/dto/addAccountDto.ts
+++ src/api/account/dto/addAccountDto.ts
@@ -1,9 +1,21 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 import { Entity } from "typeorm";
 
 @Entity('account')
 export default class AddAccountDto {
   @IsNotEmpty()
   @IsString()
+  accountNum!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name!: string;
+
+  @IsNotEmpty()
+  @IsString()
   password!: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  pay!: number;
 }
