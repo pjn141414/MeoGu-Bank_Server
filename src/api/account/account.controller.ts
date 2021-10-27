63eae58ed7f60c23201613c7172bf500449da772
pjn141414 <jina071049@gmail.com>
2021-10-27 20:42:21 +0900

    Add: addAccount and sumByuserPhone

--- src/api/account/account.controller.ts
+++ src/api/account/account.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, HttpCode, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Token } from 'src/lib/token/tokenDeco';
 import AuthGuard from 'src/middleware/auth.middleware';
 import Account from 'src/models/account';
 import User from 'src/models/User';
 import { AccountService } from './account.service';
+import AddAccountDto from './dto/addAccountDto';
 import CreateAccountDto from './dto/createAccountDto';
 
 @ApiTags('Account')
@@ -28,6 +29,21 @@ export class AccountController {
     };
   }
 
+  @Get('/find/other')
+  @HttpCode(200)
+  @UseGuards(new AuthGuard())
+  async getOtherAccountsByUserPhone(@Token() user: User) {
+    const account = await this.accountService.getOtherAccountsByUserPhone(user);
+
+    return {
+      status: 200,
+      message: '전화번호로 해당 유저의 타은행 계좌 조회 성공',
+      data: {
+        account,
+      },
+    };
+  }
+
   @Get('/find/:phone')
   @HttpCode(200)
   async getAccountsByUserPhone(@Param('phone') userPhone: string) {
@@ -35,7 +51,7 @@ export class AccountController {
 
     return {
       status: 200,
-      message: '전화번호로 해당 유저의 계좌 조회 성공',
+      message: '전화번호로 해당 유저의 자은행 계좌 조회 성공',
       data: {
         account,
       },
@@ -57,6 +73,21 @@ export class AccountController {
     };
   }
 
+  @Post('/add')
+  @HttpCode(200)
+  @UseGuards(new AuthGuard())
+  async addAccount(@Token() user: User, @Body() addAccountDto: AddAccountDto) {
+    const account: string = await this.accountService.addAccount(user, addAccountDto);
+
+    return {
+      status: 200,
+      message: '계좌 추가 성공',
+      data: {
+        account,
+      }
+    };
+  }
+
   @Get('/hold/bank')
   @HttpCode(200)
   async getMeoguHold() {
@@ -67,7 +98,23 @@ export class AccountController {
       message: '자은행 총 보유 금액 조회 성공',
       data: {
         pay,
-      }
+      },
     };
   }
+
+  @Get('/hold/bank/:phone')
+  @HttpCode(200)
+  async getMeoguHoldByUserPhone(@Param('phone') phone: string) {
+    const pay: Account[] = await this.accountService.getMeoguHoldByUserPhone(phone);
+
+    return {
+      status: 200,
+      message: '자은행 고객별 총 보유 금액 조회 성공',
+      data: {
+        pay,
+      },
+    };
+  }
+
+
 }
--- src/api/account/account.service.ts
+++ src/api/account/account.service.ts
@@ -1,6 +1,7 @@
-import { HttpService } from '@nestjs/axios';
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import axios from 'axios';
+import { DealType } from 'src/lib/enum/dealType';
+import { EndPoints } from 'src/lib/enum/endPoints';
 import uuid from 'src/lib/uuid/uuid';
 import Account from 'src/models/account';
 import User from 'src/models/User';
@@ -14,8 +15,6 @@ export class AccountService {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly accountRepository: AccountRepository,
-    @InjectRepository(Account)
-    private httpService: HttpService,
   ) { }
 
   /**
@@ -27,28 +26,7 @@ export class AccountService {
   }
 
   /**
-   * @description 유저 전화번호로 해당 유저의 자은행 계좌 조회
-   */
-  async getAccountsByUserPhone(phone: string): Promise<Account[]> {
-    const userPhone: User | undefined = await this.userRepository.getUserByPhone(phone);
-    if (userPhone === undefined) {
-      throw new NotFoundException('해당 전화번호를 가진 유저가 없습니다.');
-    }
-
-    console.log(userPhone);
-    console.log(userPhone.id);
-
-    const userId: string = userPhone.id;
-
-    console.log(userId);
-
-    const account: Account[] = await this.accountRepository.getAccountsByUserId(userId);
-
-    return account;
-  }
-
-  /**
-   * @description 계좌번호로 특정 계좌 조회
+   * @description 계좌번호로 특정 자은행 계좌 조회
    */
   async getAccountByAccountNum(accountNum: string): Promise<Account | undefined> {
     const account: Account | undefined = await this.accountRepository.getAccountByAccountNum(accountNum);
@@ -60,27 +38,33 @@ export class AccountService {
   }
 
   /**
-   * @description 타은행 계좌 조회
+   * @description 유저 전화번호로 해당 유저의 자은행 계좌 조회
    */
-  async getAccountInOther(user: User, addAccountDto: AddAccountDto): Promise<Account> {
-    const { password }: { password: string } = addAccountDto;
-
-    const userPhone: User | undefined = await this.userRepository.getUserByPhone(user.phone);
-
-    const regex = /^[0-9]{4}$/;
-    if (!regex.test(password)) {
-      throw new BadRequestException('계좌 비밀번호는 숫자 4자리입니다.');
+  async getAccountsByUserPhone(phone: string): Promise<Account[]> {
+    const userPhone: User | undefined = await this.userRepository.getUserByPhone(phone);
+    if (userPhone === undefined) {
+      throw new NotFoundException('해당 전화번호를 가진 유저가 없습니다.');
     }
 
-    this.httpService
+    const userId: string = userPhone.id;
+    const account: Account[] = await this.accountRepository.getAccountsByUserId(userId);
 
-
-    return;
+    return account;
   }
 
   /**
-   * @description 타은행 계좌 추가
+   * @description 유저 전화번호로 해당 유저의 타은행 계좌 조회
    */
+  async getOtherAccountsByUserPhone(user: User): Promise<any> {
+    const userPhone: User | undefined = await this.userRepository.getUserByPhone(user.phone);
+    if (userPhone.phone === undefined) {
+      throw new NotFoundException('해당 전화번호를 가진 유저가 없습니다.');
+    }
+
+    const account = await axios.get(`${EndPoints.SC}/communication/${userPhone.phone}`);
+
+    return account.data;
+  }
 
   /**
    * @description 계좌 개설
@@ -125,9 +109,41 @@ export class AccountService {
   /**
   * @description 계좌 추가
   */
-  // async addAccount(): Promise<Account> {
+  async addAccount(user: User, addAccountDto: AddAccountDto): Promise<string> {
+    const { accountNum, name, password, pay }:
+      { accountNum: string, name: string, password: string, pay: number } = addAccountDto;
 
-  // }
+    const userPhone: User | undefined = await this.userRepository.getUserByPhone(user.phone);
+    if (userPhone === undefined) {
+      throw new NotFoundException('해당 전화번호를 가진 유저가 없습니다.');
+    }
+
+    const checkAccount = accountNum.slice(0, 3);
+    if (checkAccount.length < 3) {
+      throw new BadRequestException('올바르지 않은 계좌번호입니다.');
+    }
+
+    while (true) {
+      const existAccount: Account | undefined = await this.accountRepository.getAccountByAccountNum(accountNum);
+      if (existAccount !== undefined) {
+        continue;
+      }
+
+      break;
+    }
+
+    const account = this.accountRepository.create({
+      accountNum: accountNum,
+      name: name,
+      password: password,
+      pay: pay,
+    });
+    account.user = userPhone;
+
+    await this.accountRepository.save(account);
+
+    return account.accountNum;
+  }
 
   /**
   * @description 자은행 총 보유 금액 조회
@@ -139,9 +155,21 @@ export class AccountService {
   /**
    * @description 자은행 고객별 총 보유 금액 조회
    */
+  async getMeoguHoldByUserPhone(phone: string): Promise<Account[]> {
+    const user: User | undefined = await this.userRepository.getUserByPhone(phone);
+    if (user === undefined) {
+      throw new NotFoundException('해당 전화번호를 가진 유저가 없습니다.');
+    }
+
+    const account: Account[] = await this.accountRepository.getMeoguHoldByUserId(user.id);
+
+    return account;
+  }
 
 
   /**
   * @description 타은행 본인 계좌 보유 금액 가져오기
   */
+
+
 }
