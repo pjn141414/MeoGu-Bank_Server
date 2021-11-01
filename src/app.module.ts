36e4f68fafc55ea1b64d5416828da0028f6c515a
pjn141414 <jina071049@gmail.com>
2021-11-01 21:47:38 +0900

    Add: Transaction Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'src/config/ormConfig';
 import { AccountModule } from './api/account/account.module';
 import { UserModule } from './api/user/user.module';
-import { DealModule } from './api/deal/deal.module';
+import { TransactionModule } from './api/transaction/transaction.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), AuthModule, AccountModule, UserModule, DealModule],
+  imports: [TypeOrmModule.forRoot(config), AuthModule, AccountModule, UserModule, TransactionModule],
   controllers: [AppController, UploadController],
   providers: [AppService, {
     provide: APP_FILTER,
