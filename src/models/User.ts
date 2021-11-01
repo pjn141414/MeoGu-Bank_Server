57b539ec0da7c924c676e6fde02f4d228854aecf
pjn141414 <jina071049@gmail.com>
2021-11-01 21:46:28 +0900

    Add: swagger @ApiProperty

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   Column,
   Entity,
@@ -10,28 +11,34 @@ import EasyPassword from "./easyPassword";
 
 @Entity('user')
 export default class User {
-
+  @ApiProperty({ description: '아이디' })
   @PrimaryColumn({ name: 'id' })
   id!: string;
 
+  @ApiProperty({ description: '비밀번호' })
   @Column({ name: 'password' })
   password!: string;
 
+  @ApiProperty({ description: '이름(실명)' })
   @Column({ name: 'name' })
   name!: string;
 
+  @ApiProperty({ description: '닉네임(가명)' })
   @Column({ name: 'nickname' })
   nickname!: string;
 
+  @ApiProperty({ description: '전화번호' })
   @Column({
     name: 'phone',
     unique: true,
   })
   phone!: string;
 
+  @ApiProperty({ description: '생년월일' })
   @Column({ name: 'birth' })
   birth!: string;
 
+  @ApiProperty({ description: '프로필 사진' })
   @Column({
     name: 'profile_image',
     nullable: true,
