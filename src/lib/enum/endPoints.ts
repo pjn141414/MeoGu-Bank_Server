b12b00c6ed7d2993426cdc061ce6884c50f336c9
pjn141414 <jina071049@gmail.com>
2021-11-06 00:30:56 +0900

    Fix: endpoints

--- src/lib/enum/endPoints.ts
+++ src/lib/enum/endPoints.ts
@@ -1,5 +1,5 @@
 export enum EndPoints {
-  JN = 'http://localhost:8000/',
+  JN = 'http://localhost:8000',
   SC = 'http://10.80.162.195:8000',
   SD = 'http://10.80.162.103:8000',
   JW = 'http://10.80.161.192:8000',
